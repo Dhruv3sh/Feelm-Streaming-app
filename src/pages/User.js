f760d7ddbc942d78d05da4efad6af13a5c7957cd
Dhruv Sharma <112899026+Dhruv3sh@users.noreply.github.com>
2024-08-17 01:36:24 +0530

    Update User.js

--- src/pages/User.js
+++ src/pages/User.js
@@ -3,7 +3,7 @@ import React from "react";
 const User = () => {
   return (
     <div className=" h-screen bg-[url('../public/images/hero.jpg')] bg-no-repeat bg-cover bg-center flex items-center justify-center">
-      <div className=" h-[480px] w-[400px] max-sm:h-[420px] max-sm:w-[330px] bg-black opacity-85 shadow-[0px_0px_21px_13px_#4a5568]">
+      <div className=" h-[480px] w-[400px] max-sm:h-[490px] max-sm:w-[330px] bg-black opacity-85 shadow-[0px_0px_21px_13px_#4a5568]">
         <div className="h-full w-full flex flex-col justify-center items-center relative">
           <div className=" absolute top-14">
             <img
