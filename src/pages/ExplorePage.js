6e5a33f4e5f11a635bf73355484a944f030ebc26
Dhruv Sharma <112899026+Dhruv3sh@users.noreply.github.com>
2024-08-23 01:40:47 +0530

    Update ExplorePage.js

--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -16,9 +16,14 @@ const ExplorePage = () => {
           page: pageNo,
         },
       });
-      setData((preve) => {
-        return [...preve, ...response.data.results];
+
+      setData((prevData) => {
+        const newData = response.data.results.filter(
+          (newItem) => !prevData.some((prevItem) => prevItem.id === newItem.id)
+        );
+        return [...prevData, ...newData];
       });
+
       setTotalPageNo(response.data.total_pages);
     } catch (error) {
       console.log("error", error);
@@ -30,15 +35,15 @@ const ExplorePage = () => {
       window.innerHeight + window.scrollY >=
       document.body.offsetHeight - 100
     ) {
-      setPageNo((preve) => preve + 1);
+      setPageNo((prevPageNo) => prevPageNo + 1);
     }
   };
 
   useEffect(() => {
     const delayFetch = setTimeout(() => {
       fetchData();
-    }, pageNo > 1 ? 1000 : 0); 
-  
+    }, pageNo > 1 ? 1000 : 0);
+
     return () => clearTimeout(delayFetch);
   }, [pageNo]);
 
@@ -54,23 +59,22 @@ const ExplorePage = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
   return (
-    <div className=" bg-zinc-950 min-h-screen pt-16 ">
-      <div className=" container mx-auto">
-        <h3 className=" capitalize text-lg lg:text-xl font-semibold my-3 pl-3">
+    <div className="bg-zinc-950 min-h-screen pt-16">
+      <div className="container mx-auto">
+        <h3 className="capitalize text-lg lg:text-xl font-semibold my-3 pl-3">
           Popular {params.explore}
         </h3>
 
-        <div className="grid grid-cols-5 pl-3 gap-y-4 lg:grid-cols-4 xl:grid-cols-5 md:grid-cols-3 sm:grid-cols-3 max-sm:grid-cols-2 ">
-          {data.map((exploreData, index) => {
-            return (
-              <Card
-                data={exploreData}
-                key={exploreData.id + "exploreSEction" + index}
-                media_type={params.explore}
-              />
-            );
-          })}
+        <div className="grid grid-cols-5 pl-3 gap-y-4 lg:grid-cols-4 xl:grid-cols-5 md:grid-cols-3 sm:grid-cols-3 max-sm:grid-cols-2">
+          {data.map((exploreData, index) => (
+            <Card
+              data={exploreData}
+              key={exploreData.id + "exploreSEction" + index}
+              media_type={params.explore}
+            />
+          ))}
         </div>
       </div>
     </div>
