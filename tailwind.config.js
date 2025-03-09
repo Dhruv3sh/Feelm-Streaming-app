9430754fc0db8e15075d43cb10b5f206d13894c8
dhruv sharma <dhruvsharma.main@gmail.com>
2025-03-09 17:12:00 +0530

    Add: screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '480px',  // Custom breakpoint for small devices
+        '3xl': '1600px', // Custom breakpoint for large screens
+        '4xl': '2560px'
+      },
+    },
   },
   darkMode: "dark",
   plugins: [nextui()],
