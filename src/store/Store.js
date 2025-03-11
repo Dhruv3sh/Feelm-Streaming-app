f52cf0998f514f1f5ce88d1a472e0b5ebff263eb
dhruv sharma <dhruvsharma.main@gmail.com>
2025-03-12 01:09:51 +0530

    fix: removed bannerdata

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import FeelmReducer from './FeelmSlice'
 import authReducer from './authSlice'
 
 export const store = configureStore({
   reducer: {
-    FeelmData : FeelmReducer,
     auth: authReducer,
   },
 })
