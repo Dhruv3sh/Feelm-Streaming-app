71d3dfcef42cea6d1457646d01ee3f11285500d1
dhruv sharma <dhruvsharma.main@gmail.com>
2025-03-17 23:03:18 +0530

    fix: minor fixes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,50 +5,45 @@ import "keen-slider/keen-slider.min.css";
 import { useKeenSlider } from "keen-slider/react";
 
 const Autoplay = (slider, { delay = 4000 } = {}) => {
-    let timeout;
-    let isMouseOver = false;
-    function clearNextTimeout() {
-      clearTimeout(timeout);
-    }
-    function nextTimeout() {
-      clearTimeout(timeout);
-      if (isMouseOver) return;
-      timeout = setTimeout(() => {
-        slider.next();
-      }, delay);
-    }
-    slider.on("created", () => {
-        // Start autoplay on creation
-        nextTimeout();
-      });
-    slider.on('dragStarted', clearNextTimeout);
-    slider.on('animationEnded', nextTimeout);
-    slider.on('updated', nextTimeout);
-  };
+  let timeout;
+  let isMouseOver = false;
+  function clearNextTimeout() {
+    clearTimeout(timeout);
+  }
+  function nextTimeout() {
+    clearTimeout(timeout);
+    if (isMouseOver) return;
+    timeout = setTimeout(() => {
+      slider.next();
+    }, delay);
+  }
+  slider.on("created", () => {
+    // Start autoplay on creation
+    nextTimeout();
+  });
+  slider.on("dragStarted", clearNextTimeout);
+  slider.on("animationEnded", nextTimeout);
+  slider.on("updated", nextTimeout);
+};
 
 const Banner = ({ trendingMovie }) => {
   const [isLoaded, setIsLoaded] = useState(false);
-  const [sliderRef, instanceRef] = useKeenSlider({
-    loop: true,
-    easing: 'ease-out',
-    dragStartThreshold: 1, // removes drag delay
-  },
-  [Autoplay]
+  const [sliderRef] = useKeenSlider(
+    {
+      loop: true,
+      easing: "ease-out",
+      dragStartThreshold: 1, // removes drag delay
+    },
+    [Autoplay]
   );
 
-  const handleImageLoad = useCallback(() => {
-    setIsLoaded(true);
-  }, []);
-
-  if (!trendingMovie || trendingMovie.length === 0) {
+  if (trendingMovie.length === 0) {
     return <Loading />;
   }
 
-  return trendingMovie ? (
+  return (
     <section ref={sliderRef}>
-      <div
-        className="flex min-h-full max-h-[98vh] overflow-hidden"
-      >
+      <div className="flex min-h-full max-h-[98vh] overflow-hidden">
         {trendingMovie.map((data, index) => {
           return (
             <div
@@ -56,17 +51,19 @@ const Banner = ({ trendingMovie }) => {
               className="keen-slider__slide min-w-full min-h-[450px] lg:min-h-full overflow-hidden relative"
             >
               <div className="w-full h-full">
-                {data?.backdrop_path && (
+                {data?.backdrop_path ? (
                   <img
                     src={
                       "https://image.tmdb.org/t/p/w1280" + data?.backdrop_path
                     }
-                    onLoad={handleImageLoad}
-                    className={`h-full w-full object-cover ${
+                    onLoad={()=>setIsLoaded(true)}
+                    className={`h-full w-full object-cover transition-opacity ${
                       isLoaded ? " opacity-100" : " opacity-0"
                     } `}
                     alt="images"
                   />
+                ) : (
+                  <Loading />
                 )}
               </div>
 
@@ -96,8 +93,6 @@ const Banner = ({ trendingMovie }) => {
         })}
       </div>
     </section>
-  ) : (
-    <Loading />
   );
 };
 
