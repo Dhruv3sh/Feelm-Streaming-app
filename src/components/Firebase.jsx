2be3c0a455befcd40c5a828b1ebd859d6d8cd1e1
dhruv sharma <dhruvsharma.main@gmail.com>
2025-04-04 12:14:36 +0530

    fix: minor fix

--- src/components/Firebase.jsx
+++ src/components/Firebase.jsx
@@ -4,11 +4,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_KEY,
   authDomain: "feelm-app.firebaseapp.com",
@@ -19,9 +15,7 @@ const firebaseConfig = {
   measurementId: "G-F8YT22CXWD"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth =getAuth();
 export const db =getFirestore(app);
 export const storage=getStorage();
